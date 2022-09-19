d21e7a880ccceebf40deea2784699312dc652e16
jinhyung.noh <jinhyung.noh@voyagerx.com>
2022-09-19 09:12:11 +0900

    nested url and link

---
+++ src/pages/company/history.js
@@ -0,0 +1,13 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const history = () => {
+  return (
+    <div>
+      <h2>this is history of our company</h2>
+      <Link to="/">home page</Link>
+    </div>
+  )
+}
+
+export default history
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
 
 // 모든 페이지 컴포넌트는 default keyword 붙어야 함
+// Link는 프로젝트 내부, a tag는 외부 리소스 접근시 사용
 export default function Home() {
-  return <div>Hello People!</div>
+  return (
+    <div>
+      <h1>Hello People!</h1>
+      <div>
+        <Link to="/about">ABOUT</Link>
+      </div>
+      <div>
+        <Link to="/company/history">history</Link>
+      </div>
+      <div>
+        <Link to="/about">ABOUT</Link>
+      </div>
+      <a href="/about">reg link</a>
+    </div>
+  )
 }
