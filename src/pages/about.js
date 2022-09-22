87d007f3ed9d40b1a5224a286cf70c83916b638e
jinhyung.noh <jinhyung.noh@voyagerx.com>
2022-09-22 09:03:28 +0900

    StaticImage and about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,38 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const About = () => {
   return (
     <Layout>
-      <h1>about page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Elit ea fugiat labore laborum.</h2>
+            <p>
+              In nisi culpa sit mollit elit laboris ut reprehenderit fugiat
+              consequat irure nostrud. In proident eu nisi commodo elit. In id
+              sunt dolor amet eu nostrud. Et culpa irure commodo aliquip veniam
+              dolor dolore do irure est adipisicing magna et.
+            </p>
+            <p>
+              Cupidatat laboris velit irure et. Esse nostrud dolore adipisicing
+              deserunt quis aute ex eu excepteur. Amet tempor sunt culpa elit
+              nostrud ad. In minim reprehenderit ex laborum mollit minim.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring Salt in Bowl"
+            className="about-img"
+            placeholder="blurred"
+          ></StaticImage>
+        </section>
+      </main>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-// 모든 페이지 컴포넌트는 default keyword 붙어야 함
-// Link는 프로젝트 내부, a tag는 외부 리소스 접근시 사용
 const Home = () => {
   return (
     <Layout>
