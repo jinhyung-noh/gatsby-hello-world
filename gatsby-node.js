76619d3d96d8d1c8ffff1cbe741054dd9f8e59e2
jinhyung.noh <jinhyung.noh@voyagerx.com>
2022-09-25 18:01:18 +0900

    finish tags page by build each recipes

---
+++ gatsby-node.js
@@ -0,0 +1,35 @@
+const path = require("path")
+/**
+ * gatsby node로 동적 페이지 생성하려면 root 위치에 있어야 함
+ * gatsby가 대신 해주는 것을 직접 해주는 과정: 데이터 받아 -> 페이지 생성
+ * 수정할때마다, 개츠비 서버 재실행해야함
+ */
+exports.createPages = async ({ graphql, actions }) => {
+  // 일단 query로 데이터를 받아와야 함(이전 방법에서는 gatsby가 해줌)
+  const { createPage } = actions
+  const result = await graphql(`
+    query getRecipes {
+      allContentfulRecipe {
+        nodes {
+          content {
+            tags
+          }
+        }
+      }
+    }
+  `)
+
+  // 받아온 데이터를 바탕으로 페이지 생성
+  result.data.allContentfulRecipe.nodes.forEach(recipe => {
+    recipe.content.tags.forEach(tag => {
+      // 페이지 생성시, 옵션을 직접 지정해주어야 함
+      createPage({
+        path: `/${tag}`,
+        component: path.resolve(`src/templates/tag-template.js`),
+        context: {
+          tag,
+        },
+      })
+    })
+  })
+}
---
+++ src/templates/tag-template.js
@@ -0,0 +1,40 @@
+import { graphql } from "gatsby"
+import React from "react"
+import RecipesList from "../components/RecipesList"
+import Layout from "../components/Layout"
+
+const TagTemplate = ({ data, pageContext }) => {
+  const recipes = data.allContentfulRecipe.nodes
+
+  return (
+    <Layout>
+      <main className="page">
+        <h2>{pageContext.tag}</h2>
+        <div className="tag-recipes">
+          <RecipesList recipes={recipes}></RecipesList>
+        </div>
+      </main>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query getRecipeByTag($tag: String) {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+      nodes {
+        title
+        id
+        prepTime
+        cookTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
+
+export default TagTemplate
